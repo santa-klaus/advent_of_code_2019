d1da902103ca140d0f904b04f239e5bc6d9571ee
santa-klaus <10899463+santa-klaus@users.noreply.github.com>
2019-12-04 22:59:47 +0100

    dec 3 part 1

--- dec3.py
+++ dec3.py
@@ -4,21 +4,38 @@ import numpy as np
 with open('dec3_input.txt','r') as fin:
     data = fin.read().splitlines()
 
+# data = ['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83']
 # Separate the two cables
-cable1 = [x.strip() for x in data[0].split(',')]
-cable2 = [x.strip() for x in data[1].split(',')]
+cable1_direc = [x.strip() for x in data[0].split(',')]
+cable2_direc = [x.strip() for x in data[1].split(',')]
 
-for k in range(len(cable1)):
-    if 'R' in cable1[k]:
-        cable1[k] = np.complex(int(cable1[k][1:]),0)
-    elif 'L' in cable1[k]:
-        cable1[k] = np.complex(-int(cable1[k][1:]),0)
-    elif 'U' in cable1[k]:
-        cable1[k] = np.complex(0,int(cable1[k][1:]))
-    elif 'D' in cable1[k]:
-        cable1[k] = np.complex(0,-int(cable1[k][1:]))
-    else:
-        print('Unknown direction:')
-        print(cable1[k])
+# Define cable directions function
+def cable_direction(cable_coords,cable_direc):
+    for k in range(len(cable_direc)):
+        if 'R' in cable_direc[k]:
+            cable_coords.extend([(cable_coords[-1][0]+x+1,cable_coords[-1][1]) for x in range(int(cable_direc[k][1:]))])
+        elif 'L' in cable_direc[k]:
+            cable_coords.extend([(cable_coords[-1][0]-x-1,cable_coords[-1][1]) for x in range(int(cable_direc[k][1:]))])
+        elif 'U' in cable_direc[k]:
+            cable_coords.extend([(cable_coords[-1][0],cable_coords[-1][1]+x+1) for x in range(int(cable_direc[k][1:]))])
+        elif 'D' in cable_direc[k]:
+            cable_coords.extend([(cable_coords[-1][0],cable_coords[-1][1]-x-1) for x in range(int(cable_direc[k][1:]))])
+        else:
+            print('Unknown direction:')
+            print(cable1[k])
 
-        
+
+cable1 = [(0,0)]
+cable2 = [(0,0)]
+
+cable_direction(cable1, cable1_direc)
+cable_direction(cable2, cable2_direc)
+
+crossings = list(set(cable1).intersection(set(cable2)).symmetric_difference([(0,0)]))
+distance = [abs(x)+abs(y) for (x,y) in crossings]
+
+print('Closest intersection:')
+print(crossings[distance.index(min(distance))])
+print('Distance of closest intersection:')
+print((min(distance)))
+
